dbc4cb017d0aa9b88b625ffc5f5edab7e19f91a4
David Hugentobler <davidhugentobler30@gmail.com>
2021-03-18 23:03:13 -0600

    creates top-level component

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <p>hello world</p>
+        <!-- <GetRandomNumber /> -->
+    </div>
+</template>
+
+<script>
+import { GetRandomNumber } from './components/GetRandomNumber'
+
+export default {
+    name: 'App',
+    components: {
+        GetRandomNumber
+    }
+}
+</script>
+
+<style>
+#app {
+    text-align: center;
+    margin-top: 100px;
+}
+</style>
