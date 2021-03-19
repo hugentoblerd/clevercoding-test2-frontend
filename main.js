7e39678c7d843762e4773aa7c4997d86853cc99a
David Hugentobler <davidhugentobler30@gmail.com>
2021-03-19 02:34:10 -0600

    creates vue instance where you can add random numbers from an api endpoint on button click. creates component and adds each new random number to a grid.

--- main.js
+++ main.js
@@ -1,24 +1,49 @@
+Vue.component('number-button', {
+    props: ['number'],
+    template: `
+    <div class="content">
+        <a href="#" @click="$emit('update-number', number)">{{ number }}</a>
+    </div>
+    `
+})
+
 new Vue({
     el: '#app',
-    components: { },
     data() {
         return {
-            number: false
+            selectedNumber: false,
+            allNumbers: []
         }
     },
     template: `
         <div id="app">
-            <h1 v-if="number">Random Number {{ number }}</h1>
-            <h1 v-else>No Random Number</h1>
+            <h1 v-if="selectedNumber">The Selected Number is {{ selectedNumber }}</h1>
+            <h1 v-else>No Number Selected</h1>
 
             <button @click="getRandomNumber">Get Random #</button>
+
+            <div class="container">
+                <number-button
+                    :class="{active: number === selectedNumber}"
+                    v-for="number in allNumbers"
+                    @update-number="updateSelected"
+                    :key="number" :number="number"
+                />
+            </div>
         </div>
     `,
     methods: {
         getRandomNumber: function () {
             fetch('http://localhost:8080/number')
                 .then(response => response.json())
-                .then(data => this.number = data.results);
+                .then(data => {
+                    this.selectedNumber = data.results;
+                    
+                    !this.allNumbers.includes(this.selectedNumber) ? this.allNumbers.push(this.selectedNumber) : '';
+                });
+        },
+        updateSelected: function (newNumber) {
+            this.selectedNumber === newNumber ? this.selectedNumber = false : this.selectedNumber = newNumber;
         }
     }
 })
